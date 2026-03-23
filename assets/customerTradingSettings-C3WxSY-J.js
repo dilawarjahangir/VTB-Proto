25097de61f67d278064297290c56f7486e8df913
Dilawar Jahangir <110585590+dilawarjahangir@users.noreply.github.com>
2026-03-23 12:40:06 +0000

    Add proto SPA files and remove tmp directory

---
+++ assets/customerTradingSettings-C3WxSY-J.js
@@ -0,0 +1,1 @@
+import{v as t}from"./index-BlUBE7RG.js";function n(){return t("/customer/trading-settings")}function s(e){return t("/customer/trading-settings",{method:"PUT",body:e})}export{n as g,s as u};
---
+++ assets/systemDefaults-knxE73rk.js
@@ -0,0 +1,1 @@
+import{v as t}from"./index-BlUBE7RG.js";function u(){return t("/admin/defaults")}function s(e){return t("/admin/defaults",{method:"PUT",body:e})}export{u as g,s as u};
